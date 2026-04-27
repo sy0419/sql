1d0f3cddc4250eacdcc49aa5caa6b96d023ea1a1
sy0419 <psy041912@gmail.com>
2026-04-27 17:35:45 +0900

    Implements a query to sort animals by name in ascending order and by intake datetime in descending order for duplicates.

--- level1/Ex10_SortByNameThenDateDesc.sql
+++ level1/Ex10_SortByNameThenDateDesc.sql
@@ -1,9 +1,9 @@
 -- 여러 기준으로 정렬하기
+
 -- ANIMAL_INS 테이블은 동물 보호소에 들어온 동물의 정보를 담은 테이블입니다. 
 -- ANIMAL_INS 테이블 구조는 다음과 같으며, 
 -- ANIMAL_ID, ANIMAL_TYPE, DATETIME, INTAKE_CONDITION, NAME, SEX_UPON_INTAKE는 
 -- 각각 동물의 아이디, 생물 종, 보호 시작일, 보호 시작 시 상태, 이름, 성별 및 중성화 여부를 나타냅니다.
-
 --       NAME	        TYPE	    NULLABLE
 --    ANIMAL_ID	      VARCHAR(N)	 FALSE
 --    ANIMAL_TYPE	  VARCHAR(N)	 FALSE
@@ -16,16 +16,15 @@
 
 -- 예시
 -- 예를 들어, ANIMAL_INS 테이블이 다음과 같다면
-
 -- ANIMAL_ID	ANIMAL_TYPE	        DATETIME	    INTAKE_CONDITION	NAME	SEX_UPON_INTAKE
 --  A349996      	Cat	      2018-01-22 14:32:00	     Normal	        Sugar	 Neutered Male
 --  A350276	        Cat	      2017-08-13 13:50:00	     Normal	        Jewel	 Spayed Female
 --  A396810	        Dog	      2016-08-22 16:13:00	    Injured	        Raven	 Spayed Female
 --  A410668	        Cat	      2015-11-19 13:41:00	     Normal	        Raven	 Spayed Female
+
 -- 이름을 사전 순으로 정렬하면 다음과 같으며, 'Jewel', 'Raven', 'Sugar'
 -- 'Raven'이라는 이름을 가진 개와 고양이가 있으므로, 이 중에서는 보호를 나중에 시작한 개를 먼저 조회합니다.
 -- 따라서 SQL문을 실행하면 다음과 같이 나와야 합니다.
-
 -- ANIMAL_ID	NAME	     DATETIME
 --  A350276	    Jewel	2017-08-13 13:50:00
 --  A396810	    Raven	2016-08-22 16:13:00
