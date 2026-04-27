8a56679c5bdf5f8ccaefe9dbd91645e3f8fc7e85
sy0419 <psy041912@gmail.com>
2026-04-27 17:39:25 +0900

    Implements a query to retrieve female patients aged 12 or younger, replacing null phone numbers with 'NONE', and sorting by age in descending order and name in ascending order.

--- level1/Ex14_FemalePatientsUnder12.sql
+++ level1/Ex14_FemalePatientsUnder12.sql
@@ -1,14 +1,15 @@
 -- 12세 이하인 여자 환자 목록 출력하기
+
 -- 다음은 종합병원에 등록된 환자정보를 담은 PATIENT 테이블입니다. 
 -- PATIENT 테이블은 다음과 같으며 PT_NO, PT_NAME, GEND_CD, AGE, TLNO는 
 -- 각각 환자번호, 환자이름, 성별코드, 나이, 전화번호를 의미합니다.
-
 -- Column name	   Type	      Nullable
 --    PT_NO	    VARCHAR(10)	   FALSE
 --   PT_NAME	VARCHAR(20)	   FALSE
 --   GEND_CD	VARCHAR(1)	   FALSE
 --     AGE 	     INTEGER	   FALSE
 --    TLNO	    VARCHAR(50)	   TRUE
+
 -- 문제
 -- PATIENT 테이블에서 12세 이하인 여자환자의 환자이름, 환자번호, 성별코드, 나이, 전화번호를 조회하는 SQL문을 작성해주세요. 
 -- 이때 전화번호가 없는 경우, 'NONE'으로 출력시켜 주시고 결과는 나이를 기준으로 내림차순 정렬하고, 
@@ -16,7 +17,6 @@
 
 -- 예시
 -- PATIENT 테이블이 다음과 같을 때
-
 --    PT_NO	   PT_NAME	GEND_CD	  AGE	  TLNO
 -- PT22000003	브라운	   M	   18	01031246641
 -- PT22000004	크롱	   M	   7	   NULL
@@ -28,8 +28,8 @@
 -- PT22000018	안나	   W	   11	   NULL
 -- PT22000019	바라	   W	   10	01079068799
 -- PT22000021	릴로	   W	   33	01023290767
--- SQL을 실행하면 다음과 같이 출력되어야 합니다.
 
+-- SQL을 실행하면 다음과 같이 출력되어야 합니다.
 -- PT_NAME	  PT_NO	    GEND_CD	  AGE	   TLNO
 --   한나	PT22000009	   W	   12	01032323117
 --   안나	PT22000018	   W	   11	   NONE
