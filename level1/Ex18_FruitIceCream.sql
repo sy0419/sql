2725fe6be5abc561829d219cb737a65620cd91f7
sy0419 <psy041912@gmail.com>
2026-04-27 17:42:00 +0900

    Implements a query to retrieve fruit-based ice creams with total orders above 3000, sorted in descending order of total orders.

--- level1/Ex18_FruitIceCream.sql
+++ level1/Ex18_FruitIceCream.sql
@@ -2,7 +2,6 @@
 
 -- 다음은 아이스크림 가게의 상반기 주문 정보를 담은 FIRST_HALF 테이블과 아이스크림 성분에 대한 정보를 담은 
 -- ICECREAM_INFO 테이블입니다. 
-
 -- FIRST_HALF 테이블 구조는 다음과 같으며, SHIPMENT_ID, FLAVOR, TOTAL_ORDER 는 
 -- 각각 아이스크림 공장에서 아이스크림 가게까지의 출하 번호, 아이스크림 맛, 상반기 아이스크림 총주문량을 나타냅니다. 
 -- FIRST_HALF 테이블의 기본 키는 FLAVOR입니다.
